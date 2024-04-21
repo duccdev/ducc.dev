667ace50dd0a4e99e79f306cb907dbc8eba08ab9
duccybaka <usr.bin.ducc.2@gmail.com>
2024-04-21 21:49:29 +0300

    feat: add github to socials

--- src/routes/socials/+page.svelte
+++ src/routes/socials/+page.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
   import Discord from "$lib/components/icons/Discord.svelte";
   import YouTube from "$lib/components/icons/YouTube.svelte";
+  import GitHub from "$lib/components/icons/GitHub.svelte";
   import Link from "$lib/components/ui/Link.svelte";
   import Code from "$lib/components/ui/Code.svelte";
 
   let discordHovered = false;
   let youtubeHovered = false;
+  let githubHovered = false;
 </script>
 
 <div class="absolute-center">
@@ -41,16 +43,13 @@
     <div>
       <!-- svelte-ignore a11y-no-static-element-interactions -->
       <div
-        class="flex items-center"
+        class="flex items-center mb-1"
         on:mouseenter={() => (youtubeHovered = true)}
         on:mouseleave={() => (youtubeHovered = false)}
       >
         <YouTube class="mr-2.5 size-8" />
 
-        <Link
-          href="https://www.youtube.com/channel/UCxHXCBqIsIpb28kzg7oajDg"
-          class="text-xl"
-        >
+        <Link href="https://github.com/amogustroll69" class="text-xl">
           {#if youtubeHovered}
             <div class="flex items-center">
               <Code class="mr-1">amogustroll69</Code>
@@ -58,6 +57,28 @@
             </div>
           {:else}
             YouTube
+          {/if}
+        </Link>
+      </div>
+    </div>
+
+    <div>
+      <!-- svelte-ignore a11y-no-static-element-interactions -->
+      <div
+        class="flex items-center"
+        on:mouseenter={() => (githubHovered = true)}
+        on:mouseleave={() => (githubHovered = false)}
+      >
+        <GitHub class="mr-2.5 size-8" />
+
+        <Link
+          href="https://www.youtube.com/channel/UCxHXCBqIsIpb28kzg7oajDg"
+          class="text-xl"
+        >
+          {#if githubHovered}
+            <Code>amogustroll69</Code>
+          {:else}
+            GitHub
           {/if}
         </Link>
       </div>
