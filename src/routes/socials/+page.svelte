e4b0bd4377ae8e61d1da68793700d0377d728b91
duccybaka <usr.bin.ducc.2@gmail.com>
2024-04-25 00:49:40 +0300

    fix: mobile-optimize /socials

--- src/routes/socials/+page.svelte
+++ src/routes/socials/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import DesktopView from "$lib/components/ui/DesktopView.svelte";
+  import MobileView from "$lib/components/ui/MobileView.svelte";
   import Discord from "$lib/components/icons/Discord.svelte";
   import YouTube from "$lib/components/icons/YouTube.svelte";
   import GitHub from "$lib/components/icons/GitHub.svelte";
@@ -10,81 +12,140 @@
   let githubHovered = false;
 </script>
 
-<div class="absolute-center">
-  <div>
-    <div class="flex items-center">
-      <div class="border border-ctp-green w-48" />
-      <p class="text-ctp-green text-3xl mx-2">Socials</p>
-      <div class="border border-ctp-green w-48" />
-    </div>
-
+<DesktopView>
+  <div class="absolute-center">
     <div>
-      <!-- svelte-ignore a11y-no-static-element-interactions -->
-      <div
-        class="flex items-center mb-1"
-        on:mouseenter={() => (discordHovered = true)}
-        on:mouseleave={() => (discordHovered = false)}
-      >
-        <Discord class="mr-2.5 size-8" />
+      <div class="flex items-center">
+        <div class="border border-ctp-green w-48" />
+        <p class="text-ctp-green text-3xl mx-2">Socials</p>
+        <div class="border border-ctp-green w-48" />
+      </div>
 
-        <Link
-          href="https://discord.com/channels/@me/842457844724400142"
-          class="text-xl"
+      <div>
+        <!-- svelte-ignore a11y-no-static-element-interactions -->
+        <div
+          class="flex items-center mb-1"
+          on:mouseenter={() => (discordHovered = true)}
+          on:mouseleave={() => (discordHovered = false)}
         >
-          {#if discordHovered}
-            <Code>amogustroll69</Code>
-          {:else}
-            Discord
-          {/if}
-        </Link>
+          <Discord class="mr-2.5 size-8" />
+
+          <Link
+            href="https://discord.com/channels/@me/842457844724400142"
+            class="text-xl"
+          >
+            {#if discordHovered}
+              <Code>amogustroll69</Code>
+            {:else}
+              Discord
+            {/if}
+          </Link>
+        </div>
       </div>
-    </div>
 
-    <div>
-      <!-- svelte-ignore a11y-no-static-element-interactions -->
-      <div
-        class="flex items-center mb-1"
-        on:mouseenter={() => (youtubeHovered = true)}
-        on:mouseleave={() => (youtubeHovered = false)}
-      >
-        <YouTube class="mr-2.5 size-8" />
-
-        <Link
-          href="https://www.youtube.com/channel/UCxHXCBqIsIpb28kzg7oajDg"
-          class="text-xl"
+      <div>
+        <!-- svelte-ignore a11y-no-static-element-interactions -->
+        <div
+          class="flex items-center mb-1"
+          on:mouseenter={() => (youtubeHovered = true)}
+          on:mouseleave={() => (youtubeHovered = false)}
         >
-          {#if youtubeHovered}
-            <div class="flex items-center">
-              <Code class="mr-1">amogustroll69</Code>
-              <p class="text-lg">(i almost never post there)</p>
-            </div>
-          {:else}
-            YouTube
-          {/if}
-        </Link>
+          <YouTube class="mr-2.5 size-8" />
+
+          <Link
+            href="https://www.youtube.com/channel/UCxHXCBqIsIpb28kzg7oajDg"
+            class="text-xl"
+          >
+            {#if youtubeHovered}
+              <div class="flex items-center">
+                <Code class="mr-1">amogustroll69</Code>
+                <p class="text-lg">(i almost never post there)</p>
+              </div>
+            {:else}
+              YouTube
+            {/if}
+          </Link>
+        </div>
       </div>
-    </div>
 
-    <div>
-      <!-- svelte-ignore a11y-no-static-element-interactions -->
-      <div
-        class="flex items-center"
-        on:mouseenter={() => (githubHovered = true)}
-        on:mouseleave={() => (githubHovered = false)}
-      >
-        <GitHub class="mr-2.5 size-8" />
+      <div>
+        <!-- svelte-ignore a11y-no-static-element-interactions -->
+        <div
+          class="flex items-center"
+          on:mouseenter={() => (githubHovered = true)}
+          on:mouseleave={() => (githubHovered = false)}
+        >
+          <GitHub class="mr-2.5 size-8" />
 
-        <Link href="https://github.com/amogustroll69" class="text-xl">
-          {#if githubHovered}
-            <Code>amogustroll69</Code>
-          {:else}
-            GitHub
-          {/if}
-        </Link>
+          <Link href="https://github.com/duccybaka" class="text-xl">
+            {#if githubHovered}
+              <Code>duccybaka</Code>
+            {:else}
+              GitHub
+            {/if}
+          </Link>
+        </div>
       </div>
-    </div>
 
-    <div class="border border-ctp-green w-full mt-4 mb-2" />
-    <p class="text-sm">yeah there isn't much here :/</p>
+      <div class="border border-ctp-green w-full mt-4 mb-2" />
+      <p class="text-sm">yeah there isn't much here :/</p>
+    </div>
   </div>
-</div>
+</DesktopView>
+
+<MobileView>
+  <div class="absolute-center">
+    <div>
+      <p class="text-ctp-green text-3xl mb-2">Socials</p>
+
+      <div>
+        <!-- svelte-ignore a11y-no-static-element-interactions -->
+        <div
+          class="flex items-center mb-1"
+          on:mouseenter={() => (discordHovered = true)}
+          on:mouseleave={() => (discordHovered = false)}
+        >
+          <Discord class="mr-2.5 size-8" />
+
+          <Link
+            href="https://discord.com/channels/@me/842457844724400142"
+            class="text-xl"
+          >
+            <Code>amogustroll69</Code>
+          </Link>
+        </div>
+      </div>
+
+      <div>
+        <!-- svelte-ignore a11y-no-static-element-interactions -->
+        <div
+          class="flex items-center mb-1"
+          on:mouseenter={() => (youtubeHovered = true)}
+          on:mouseleave={() => (youtubeHovered = false)}
+        >
+          <YouTube class="mr-2.5 size-8" />
+
+          <Link
+            href="https://www.youtube.com/channel/UCxHXCBqIsIpb28kzg7oajDg"
+            class="text-xl"
+          >
+            <Code class="mr-1">amogustroll69</Code>
+          </Link>
+        </div>
+      </div>
+
+      <div>
+        <!-- svelte-ignore a11y-no-static-element-interactions -->
+        <div class="flex items-center">
+          <GitHub class="mr-2.5 size-8" />
+
+          <Link href="https://github.com/duccybaka" class="text-xl">
+            <Code class="mb-2">duccybaka</Code>
+          </Link>
+        </div>
+      </div>
+
+      <p class="text-sm mt-4">yeah there isn't much here :/</p>
+    </div>
+  </div>
+</MobileView>
