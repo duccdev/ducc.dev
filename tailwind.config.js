fa7c6c1a4b1cbfee894e19f85902d74f9aa74ead
duccdev <usr.bin.ducc.2@gmail.com>
2024-06-27 15:31:00 +0300

    fix: build error due to me forgetting about catppuccin tcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,8 @@
-import catppuccinTcss from "@catppuccin/tailwindcss";
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
     extend: {},
   },
-  plugins: [
-    catppuccinTcss({
-      prefix: "ctp",
-      defaultFlavour: "mocha",
-    }),
-  ],
+  plugins: [],
 };
